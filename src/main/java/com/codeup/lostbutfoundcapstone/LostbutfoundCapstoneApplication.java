a4b8a4af2b4160593e146f0e4bd0f45b174c062f
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-21 16:28:31 -0500

    feat: first commit

---
+++ src/main/java/com/codeup/lostbutfoundcapstone/LostbutfoundCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.lostbutfoundcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostbutfoundCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostbutfoundCapstoneApplication.class, args);
+    }
+
+}
