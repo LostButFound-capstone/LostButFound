706e237bd65f8bf3e65245ccf891f5e6f55d6b4f
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-22 14:48:05 -0500

    feat: added getters and setters for property class

--- src/main/java/com/codeup/lostbutfoundcapstone/DAOs/PropertyRepository.java
+++ src/main/java/com/codeup/lostbutfoundcapstone/DAOs/PropertyRepository.java
@@ -1,5 +1,8 @@
 package com.codeup.lostbutfoundcapstone.DAOs;
 
-public interface PropertyRepository {
-    //asdfasd
+import com.codeup.lostbutfoundcapstone.models.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+
 }
--- src/main/java/com/codeup/lostbutfoundcapstone/models/Property.java
+++ src/main/java/com/codeup/lostbutfoundcapstone/models/Property.java
@@ -71,5 +71,83 @@ public class Property {
         this.is_animal = is_animal;
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate_found() {
+        return date_found;
+    }
+
+    public void setDate_found(Date date_found) {
+        this.date_found = date_found;
+    }
+
+    public Date getDate_posted() {
+        return date_posted;
+    }
+
+    public void setDate_posted(Date date_posted) {
+        this.date_posted = date_posted;
+    }
+
+    public boolean isInquiry_complete() {
+        return inquiry_complete;
+    }
+
+    public void setInquiry_complete(boolean inquiry_complete) {
+        this.inquiry_complete = inquiry_complete;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public boolean isIs_animal() {
+        return is_animal;
+    }
+
+    public void setIs_animal(boolean is_animal) {
+        this.is_animal = is_animal;
+    }
+
+    public List<PropertyCategory> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<PropertyCategory> categories) {
+        this.categories = categories;
+    }
+
+    public List<Inquiry> getInquiries() {
+        return inquiries;
+    }
+
+    public void setInquiries(List<Inquiry> inquiries) {
+        this.inquiries = inquiries;
+    }
 }
