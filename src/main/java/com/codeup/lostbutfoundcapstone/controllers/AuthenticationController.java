e979da41440df7604a846ad5bf2fa0da5e409fc7
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-22 16:44:30 -0500

    feat: added html directories

--- src/main/java/com/codeup/lostbutfoundcapstone/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/lostbutfoundcapstone/controllers/AuthenticationController.java
@@ -1,13 +1,13 @@
-package com.codeup.lostbutfoundcapstone.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class AuthenticationController {
-
-    @GetMapping("/login")
-    public String showLoginForm() {
-        return "login";
-    }
-}
+//package com.codeup.lostbutfoundcapstone.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class AuthenticationController {
+//
+//    @GetMapping("/login")
+//    public String showLoginForm() {
+//        return "login";
+//    }
+//}
