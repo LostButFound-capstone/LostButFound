853f697058dc01d9b184f2879ccd0ae2660b085d
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-24 16:10:47 -0500

    feat: working on create functionality

--- src/main/java/com/codeup/lostbutfoundcapstone/DAOs/UserRepository.java
+++ src/main/java/com/codeup/lostbutfoundcapstone/DAOs/UserRepository.java
@@ -1,9 +1,8 @@
 package com.codeup.lostbutfoundcapstone.DAOs;
 
-import com.codeup.lostbutfoundcapstone.models.Property;
 import com.codeup.lostbutfoundcapstone.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Property, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 //    User findByUsername(String username);
 }
--- src/main/java/com/codeup/lostbutfoundcapstone/controllers/PropertyController.java
+++ src/main/java/com/codeup/lostbutfoundcapstone/controllers/PropertyController.java
@@ -1,6 +1,7 @@
 package com.codeup.lostbutfoundcapstone.controllers;
 
 
+import com.codeup.lostbutfoundcapstone.DAOs.LocationRepository;
 import com.codeup.lostbutfoundcapstone.DAOs.PropertyCategoryRepository;
 import com.codeup.lostbutfoundcapstone.DAOs.PropertyRepository;
 import com.codeup.lostbutfoundcapstone.DAOs.UserRepository;
@@ -16,25 +17,25 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class PropertyController {
 
     private final PropertyRepository propertyDao;
-
     private final PropertyCategoryRepository propertyCategoryDao;
-
+    private final LocationRepository locationDao;
     private final UserRepository userDao;
-
     private final EmailService emailService;
 
 
 
-    public PropertyController(PropertyRepository propertyDao, PropertyCategoryRepository propertyCategoryDao, UserRepository userDao, EmailService emailService){
-
+    public PropertyController(PropertyRepository propertyDao, PropertyCategoryRepository propertyCategoryDao, UserRepository userDao, EmailService emailService, LocationRepository locationDao){
         this.propertyDao = propertyDao;
         this.propertyCategoryDao = propertyCategoryDao;
         this.userDao =  userDao;
         this.emailService = emailService;
+        this.locationDao = locationDao;
     }
 
 
@@ -55,22 +56,25 @@ public class PropertyController {
     public String createProperty(Model model) {
         model.addAttribute("property", new Property());
         model.addAttribute("categories", propertyCategoryDao.findAll());
-        return "create";
+        model.addAttribute("locations", locationDao.findAll());
+        return "property/create";
     }
 
-//    @PostMapping("/property/create")
-//    public String createPostProperty(@ModelAttribute Property property) {
+    @PostMapping("/property/create")
+    public String createPostProperty(@ModelAttribute Property property, @ModelAttribute List) {
 //        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        property.setUser(user);
-//
-//        Property savedProperty = propertyDao.save(property);
-//
-//        emailService.prepareAndSend(property, property.getTitle(), "Check this out!");
-//
-//        return "redirect:/property/" + savedProperty.getId();
-//    }
+        User user = userDao.getById(1L);
 
-    @GetMapping("/user/{id}")
+        property.setUser(user);
+
+        Property savedProperty = propertyDao.save(property);
+
+        emailService.prepareAndSend(property, property.getTitle(), "Check this out!");
+
+        return "redirect:/property/" + savedProperty.getId();
+    }
+
+    @GetMapping("/user/profile")
     public String showProfilePage(@PathVariable long id, Model model) {
 //        (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
