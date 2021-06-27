6b92dbaf2a0073a4c41fe15623e55cc23be134a1
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-27 15:43:45 -0500

    feat: mostly finished with filter functionality

--- src/main/java/com/codeup/lostbutfoundcapstone/controllers/PropertyController.java
+++ src/main/java/com/codeup/lostbutfoundcapstone/controllers/PropertyController.java
@@ -5,6 +5,7 @@ import com.codeup.lostbutfoundcapstone.DAOs.LocationRepository;
 import com.codeup.lostbutfoundcapstone.DAOs.PropertyCategoryRepository;
 import com.codeup.lostbutfoundcapstone.DAOs.PropertyRepository;
 import com.codeup.lostbutfoundcapstone.DAOs.UserRepository;
+import com.codeup.lostbutfoundcapstone.models.Location;
 import com.codeup.lostbutfoundcapstone.models.Property;
 import com.codeup.lostbutfoundcapstone.models.PropertyCategory;
 import com.codeup.lostbutfoundcapstone.models.User;
@@ -15,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Date;
 
 @Controller
 public class PropertyController {
@@ -87,19 +89,58 @@ public class PropertyController {
     }
 
     @GetMapping("/property/listings")
-    public String showListings(Model model) {
+    public String showListings(Model model, @RequestParam(name = "location" , required = false) String location_id, @RequestParam(name = "category" , required = false) String category_id, @RequestParam(name = "date_found" , required = false) String date_found) {
         model.addAttribute("properties", propertyDao.findAll());
         model.addAttribute("locations", locationDao.findAll());
         model.addAttribute("categories", propertyCategoryDao.findAll());
 
-        return "property/listings";
+
+        if(location_id == null && category_id == null) {
+            System.out.println("both actually null");
+            model.addAttribute("properties", propertyDao.findAll());
+        } else if (location_id.equals("null") && category_id.equals("null")) {
+            System.out.println("both equal null but as a string");
+            model.addAttribute("properties", propertyDao.findAll());
+        } else if (!location_id.equals("null") && category_id.equals("null")) {
+            System.out.println("location id is not equal to null string but category id is");
+            Long locationIdNum = Long.parseLong(location_id);
+            Location location = locationDao.getById(locationIdNum);
+            model.addAttribute("properties", propertyDao.findPropertyByLocation(location));
+        } else if (!category_id.equals("null") && location_id.equals("null")) {
+            System.out.println("category id is not equal to null string but location id is");
+            Long categoryIdNum = Long.parseLong(category_id);
+            PropertyCategory category = propertyCategoryDao.getById(categoryIdNum);
+            model.addAttribute("properties", propertyDao.findPropertyByCategories(category));
+        } else if (!category_id.equals("null") && !location_id.equals("null")) {
+            System.out.println("both category and location are not null");
+            Long locationIdNum = Long.parseLong(location_id);
+            Location location = locationDao.getById(locationIdNum);
+            Long categoryIdNum = Long.parseLong(category_id);
+            PropertyCategory category = propertyCategoryDao.getById(categoryIdNum);
+            model.addAttribute("properties", propertyDao.findPropertyByCategoriesAndLocation(category, location));
+        }
+
+        System.out.println(location_id);
+        System.out.println(category_id);
+
+        return "property/listings-dummy";
     }
 
-    @PostMapping("/property/listings")
-    public String filterFunctionality(@RequestParam(name = "location") String location_id, @RequestParam(name = "category") String category_id, @RequestParam(name = "date_found") String date_found) {
+//    @PostMapping("/property/listings")
+//    public String filterFunctionality(@RequestParam(name = "location") String location_id, @RequestParam(name = "category") String category_id, @RequestParam(name = "date_found") String date_found) {
+////        Long locationIdNum = Long.parseLong(location_id);
+////        Long categoryIdNum = Long.parseLong(category_id);
+//
+//
+//        return "redirect:/property/listings";
+//    }
 
-
-        return "redirect:/property/listings";
+    @GetMapping("/property/edit")
+    public String editProperty(Model model) {
+        model.addAttribute("property", new Property());
+        model.addAttribute("categories", propertyCategoryDao.findAll());
+        model.addAttribute("locations", locationDao.findAll());
+        return "property/edit-dummy";
     }
 
 }
