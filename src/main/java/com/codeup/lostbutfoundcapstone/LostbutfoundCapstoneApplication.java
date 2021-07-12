180c0b27181796b6884784aaf319992a20216c38
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-07-12 14:44:06 -0500

    feat: refactored filestack api controller

--- src/main/java/com/codeup/lostbutfoundcapstone/LostbutfoundCapstoneApplication.java
+++ src/main/java/com/codeup/lostbutfoundcapstone/LostbutfoundCapstoneApplication.java
@@ -1,8 +1,12 @@
 package com.codeup.lostbutfoundcapstone;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class LostbutfoundCapstoneApplication {
 
